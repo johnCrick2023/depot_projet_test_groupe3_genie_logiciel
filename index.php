8b8e89c36e1920aad88e2218c8c9ddf849f795c0
VITILUXX <119943526+VItiluxx@users.noreply.github.com>
2023-07-12 18:56:22 +0200

    Create index.php
    juste le squelette de notre code

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notre_site_genie_logiciel</title>
+</head>
+<body>
+    <h1>BIENVENUE SUR NOTRE PAGE DU PROJET SUR LE COURS DE GENIE LOGICIEL<h1>
+</body>
+</html>
